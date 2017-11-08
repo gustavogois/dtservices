5909f23cd23e435226eb3e5b00c5c284bb664175
Gustavo Gois <gustavo.gois@gmail.com>
2017-11-08 09:11:51 +0000

    Step 5 Salvar e buscar pelo código os Tipos de Serviço

--- src/main/java/pt/gois/dtservices/resource/TipoDeServicoResource.java
+++ src/main/java/pt/gois/dtservices/resource/TipoDeServicoResource.java
@@ -1,11 +1,19 @@
 package pt.gois.dtservices.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import pt.gois.dtservices.model.TipoDeServico;
 import pt.gois.dtservices.repository.TipoDeServicoRepository;
@@ -20,6 +28,22 @@ public class TipoDeServicoResource {
 	@GetMapping
 	public List<TipoDeServico> listar() {
 		return tipoDeServicoRepository.findAll();
+	}
+	
+	// TODO: Utilizar código e não ID para identificar unicamente as entidades
+	
+	@PostMapping
+	public ResponseEntity<TipoDeServico> criar(@RequestBody TipoDeServico tipoDeServico, HttpServletResponse response) {
+		TipoDeServico tipoDeServicoSalvo = tipoDeServicoRepository.save(tipoDeServico);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(tipoDeServicoSalvo.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(tipoDeServicoSalvo);
+	}
+	
+	@GetMapping("/{codigo}")
+	public TipoDeServico buscaPeloCodigo(@PathVariable Long codigo) {
+		return tipoDeServicoRepository.findOne(codigo);
 	}
 
 }
