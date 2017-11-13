2bc058f9037d350cdc9ca82d0f9232c783cea969
Gustavo Gois <gustavo.gois@gmail.com>
2017-11-13 22:07:47 +0000

    Step 15 POM para datas

--- src/test/java/pt/gois/dtservices/DtServicesApplicationTests.java
+++ src/test/java/pt/gois/dtservices/DtServicesApplicationTests.java
@@ -1,16 +1,40 @@
 package pt.gois.dtservices;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import pt.gois.dtservices.repository.TipoDeServicoRepository;
+import pt.gois.dtservices.resource.TipoDeServicoResource;
+import pt.gois.dtservices.service.TipoDeServicoService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(TipoDeServicoResource.class)
 public class DtServicesApplicationTests {
 
+	@Autowired
+    private MockMvc mvc;
+	
+	@MockBean
+    private TipoDeServicoRepository repository;
+	
+	@MockBean
+    private TipoDeServicoService service;
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		
+		mvc.perform(get("/tiposdeservico")
+			      .contentType(MediaType.APPLICATION_JSON))
+			      .andExpect(status().isOk());
 	}
 
 }
